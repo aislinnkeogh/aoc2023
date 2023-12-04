2bf2412144775fee5c0eea1f43de00a8e3c1e654
aislinnkeogh <a.k.keogh@sms.ed.ac.uk>
2023-12-04 17:09:53 +0000

    Update list comprehension to set intersection

--- code/script/day04.py
+++ code/script/day04.py
@@ -4,7 +4,7 @@ with open('input/day04.txt') as f:
     lines = [line.rstrip("\n") for line in f.readlines()]
 
 cards = {i+1: {"winning": lines[i].split(": ")[1].split(" | ")[0].split(), "have": lines[i].split(": ")[1].split(" | ")[1].split()} for i in range(len(lines))}
-matches = [len([e for e in cards[i+1]["winning"] if e in cards[i+1]["have"]]) for i in range(len(cards))]
+matches = [len(set(cards[i+1]["winning"]).intersection(set(cards[i+1]["have"]))) for i in range(len(cards))]
 part1 = sum([2**(m-1) for m in matches if m>0])
 
 copies = {c: 1 for c in cards}
