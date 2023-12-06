e73d725d096b117802f5d99863feb4b6be82058b
aislinnkeogh <a.k.keogh@sms.ed.ac.uk>
2023-12-06 16:45:58 +0000

    Update Day 4 input processing

--- code/script/day04.py
+++ code/script/day04.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 with open('input/day04.txt') as f:
-    lines = [line.rstrip("\n") for line in f.readlines()]
+    cards = []
+    for i, line in enumerate(l.rstrip("\n") for l in f.readlines()):
+        winning, have = line.split(": ")[1].split(" | ")
+        cards.append((winning.split(), have.split()))
 
-cards = {i+1: {"winning": line.split(": ")[1].split(" | ")[0].split(), "have": line.split(": ")[1].split(" | ")[1].split()} for i, line in enumerate(lines)}
-matches = [len(set(cards[i+1]["winning"]).intersection(set(cards[i+1]["have"]))) for i in range(len(cards))]
+matches = [len(set(cards[i][0]).intersection(set(cards[i][1]))) for i in range(len(cards))]
 part1 = sum([2**(m-1) for m in matches if m>0])
 
-copies = {c: 1 for c in cards}
+copies = {i+1: 1 for i in range(len(cards))}
 for i in range(len(cards)):
     for j in range(matches[i]):
         copies[i+j+2] += copies[i+1]
