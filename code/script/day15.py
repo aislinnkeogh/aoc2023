bd6e4e7767831c6e7007e9bfb0923802b51e861f
aislinnkeogh <a.k.keogh@sms.ed.ac.uk>
2023-12-15 16:09:54 +0000

    Update Day 15

--- code/script/day15.py
+++ code/script/day15.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from collections import defaultdict
-
 with open("input/day15.txt") as f:
     sequence = f.readlines()[0].rstrip("\n").split(",")
 
@@ -16,27 +14,27 @@ part1 = sum([HASH(step) for step in sequence])
 lenses = [(step[:-2], int(step[-1])) if step[-2]=="=" else (step[:-1], 0) for step in sequence]
 
 boxes = {i: [[],[]] for i in range(256)}
-for step in lenses:
-    labels, focal_lengths = boxes[HASH(step[0])]
-    if step[1] == 0:
+for lens in lenses:
+    labels, focal_lengths = boxes[HASH(lens[0])]
+    if lens[1] == 0:
         try:
-            idx = labels.index(step[0])
+            idx = labels.index(lens[0])
             labels.pop(idx)
             focal_lengths.pop(idx)
         except:
             pass
     else:
         try:
-            idx = labels.index(step[0])
-            focal_lengths[idx] = step[1]
+            idx = labels.index(lens[0])
+            focal_lengths[idx] = lens[1]
         except:
-            labels.append(step[0])
-            focal_lengths.append(step[1])
+            labels.append(lens[0])
+            focal_lengths.append(lens[1])
 
 part2 = 0
-for i in boxes.keys():
-    for j in range(len(boxes[i][1])):
-        part2 += (i+1) * (j+1) * boxes[i][1][j]
+for i, box in boxes.items():
+    for j, val in enumerate(box[1]):
+        part2 += (i+1) * (j+1) * val
 
 print(part1)
 print(part2)
