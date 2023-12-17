d03b6fcf20d65e1be0c8fd053590cee3865b561c
aislinnkeogh <a.k.keogh@sms.ed.ac.uk>
2023-12-17 12:36:50 +0000

    Update Day 2 without numpy

--- code/script/day02.py
+++ code/script/day02.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python
 
-import numpy as np
+from functools import reduce
 
 with open('input/day02.txt') as f:
-    lines = [line.rstrip("\n") for line in f.readlines()]
+    games = [[rnd.split(", ") for rnd in game] for game in [line.rstrip("\n").split(": ")[1].split("; ") for line in f.readlines()]]
 
-games = [line.split(": ")[1].split("; ") for line in lines]
-parsed_games = [[rnd.split(", ") for rnd in game] for game in games]
-
-all_games = {}
-for i in range(len(parsed_games)):
-    all_games[i+1] = []
-    for j in range(len(parsed_games[i])):
-        all_games[i+1].append({"red": 0, "green": 0, "blue": 0})
-        for clr in parsed_games[i][j]:
+highest_per_game = {i: {"red": 0, "green": 0, "blue": 0} for i in range(len(games))}
+for i, game in enumerate(games):
+    for rnd in game:
+        for clr in rnd:
             n, x = clr.split(" ")
-            all_games[i+1][j][x] += int(n)
-
-highest_per_game = {}
-for i in range(len(all_games)):
-    highest_per_game[i+1] = {"red": 0, "green": 0, "blue": 0}
-    for rnd in all_games[i+1]:
-        for clr in ["red", "green", "blue"]:
-            if rnd[clr] > highest_per_game[i+1][clr]:
-                highest_per_game[i+1][clr] = rnd[clr]
+            if int(n) > highest_per_game[i][x]:
+                highest_per_game[i][x] = int(n)
 
 part1 = 0
-for i in range(len(highest_per_game)):
-    if highest_per_game[i+1]["red"] <= 12 and highest_per_game[i+1]["green"] <= 13 and highest_per_game[i+1]["blue"] <= 14:
+part2 = 0
+for i, v in highest_per_game.items():
+    if v["red"] <= 12 and v["green"] <= 13 and v["blue"] <= 14:
         part1 += i+1
-
-part2 = np.sum([np.prod(list(highest_per_game[i+1].values())) for i in range(len(highest_per_game))])
+    part2 += reduce((lambda x, y: x * y), v.values())
 
 print(part1)
 print(part2)
